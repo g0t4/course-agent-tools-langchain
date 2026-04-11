6a58631c273a6966287759ae86bb34ca07181250
Wes Higbee <wes.mcclure@gmail.com>
2026-04-11 03:15:13 -0500

    run_python+run_command

--- tools/run_python.py
+++ tools/run_python.py
@@ -9,14 +9,33 @@ from show import show_messages
 
 model = ChatLlamaServer(base_url="http://paxy:8013", api_key="")
 
-def get_the_time():
-    """ Returns the local time in HH:MM AM/PM format"""
-    return datetime.now().strftime("%I:%M %p")
-    # return datetime.now() # surprise with date too
+def run_python(code: str):
+    """ Execute a python script and return STDOUT """
+    import io
+    import contextlib
+    import traceback
+    stdout = io.StringIO()
+    try:
+        with contextlib.redirect_stdout(stdout):
+            exec(code, {})
+    except Exception:
+        return traceback.format_exc()
+    return stdout.getvalue()
 
-@tool(description="get the date in YYYY-mm-dd format")
-def lookup_date():
-    return datetime.now().strftime("%Y-%m-%d")
+def run_command(commandline: str) -> dict[str, str | int]:
+    """Execute a shell commandline and return its STDOUT."""
+    import subprocess
+    result = subprocess.run(
+        commandline,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        text=True,
+    )
+    return {
+        "stdout": result.stdout,
+        "returncode": result.returncode,
+    }
 
 # %%
 
@@ -24,7 +43,7 @@ messages = [
     HumanMessage("Get me a unix timestamp for right now?"),
 ]
 
-agent = create_agent(model, tools=[get_the_time, lookup_date])
+agent = create_agent(model, tools=[run_python, run_command])
 thread = agent.invoke({"messages": messages})  # pyright: ignore
 
 show_messages(thread["messages"])
