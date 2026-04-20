1e7ffb8f63a0d38c3a07a744d7b1885260a925d8
Wes Higbee <wes.mcclure@gmail.com>
2026-04-20 16:15:14 -0500

    wiki

--- tools/wiki.py
+++ tools/wiki.py
@@ -8,44 +8,25 @@ from langchain_core.messages import AIMessage, HumanMessage, ToolMessage
 from langchain_core.tools import tool
 from langchain_llama_server import ChatLlamaServer
 
+from langchain_community.tools import WikipediaQueryRun
+from langchain_community.utilities import WikipediaAPIWrapper
 
+wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# wikipedia.run("AMD consumer CPUs")
 
 
 # %% 
 
+from run_python import run_command, run_python
 
 model = ChatLlamaServer(base_url="http://paxy:8012", api_key="")
-
 
 messages = [
     HumanMessage("What are some modern AMD consumer CPUs?"),
 ]
 
-from run_python import run_command, run_python
-
-agent = create_agent(model, tools=[run_python, run_command])
+agent = create_agent(model, tools=[run_python, run_command, wikipedia])
 
 from show import stream_messages
 await stream_messages(agent, messages) # pyright: ignore
