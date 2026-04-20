1f5e33cf359e23020540148aea88bd558334b453
Wes Higbee <wes.mcclure@gmail.com>
2026-04-20 15:34:44 -0500

    wiki

---
+++ tools/wiki.py
@@ -0,0 +1,51 @@
+# auto reload module changes
+get_ipython().extension_manager.load_extension("autoreload")  # pyright: ignore
+get_ipython().run_line_magic('autoreload', 'complete --print')  # pyright: ignore
+
+import rich
+from langchain.agents import create_agent
+from langchain_core.messages import AIMessage, HumanMessage, ToolMessage
+from langchain_core.tools import tool
+from langchain_llama_server import ChatLlamaServer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# %% 
+
+
+model = ChatLlamaServer(base_url="http://paxy:8012", api_key="")
+
+
+messages = [
+    HumanMessage("What are some modern AMD consumer CPUs?"),
+]
+
+from run_python import run_command, run_python
+
+agent = create_agent(model, tools=[run_python, run_command])
+
+from show import stream_messages
+await stream_messages(agent, messages) # pyright: ignore
