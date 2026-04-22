588e93f86d01fa9092480bf8d76c2eccc4990975
Wes Higbee <wes.mcclure@gmail.com>
2026-04-22 18:44:02 -0500

    recover

---
+++ tools/recover.py
@@ -0,0 +1,25 @@
+# auto reload module changes
+get_ipython().extension_manager.load_extension("autoreload")  # pyright: ignore
+get_ipython().run_line_magic('autoreload', 'complete --print')  # pyright: ignore
+
+import rich
+from langchain.agents import create_agent
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage, ToolMessage
+from langchain_core.tools import tool
+from langchain_llama_server import ChatLlamaServer
+
+model = ChatLlamaServer(base_url="http://paxy:8012", api_key="",
+    # Qwen3.6 via llama-server
+    # extra_body={"chat_template_kwargs": { "enable_thinking": False }}, \
+)
+
+messages = [
+    HumanMessage("Explain the project in the current directory"),
+]
+
+from run_python import run_command, run_python
+
+agent = create_agent(model, tools=[run_python, run_command])
+
+from show import stream_messages
+await stream_messages(agent, messages) # pyright: ignore
