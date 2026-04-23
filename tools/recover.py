6230532e378d38d88711fe21e35dfc5171d9b157
Wes Higbee <wes.mcclure@gmail.com>
2026-04-23 13:46:36 -0500

    better example of recovery

--- tools/recover.py
+++ tools/recover.py
@@ -17,7 +17,8 @@ messages = [
     # HumanMessage("Read show.py and investigate how I display the tool call ID value... add a function to shorten the ID to a max of first 6 chars and use that to show the ID"),
     # HumanMessage("Explain this project, in the current directory"),
     # HumanMessage("Read show.py and modify stream_messages to show the AI model used when generating a message, get this during on_chat_model_end from response metadata"),
-    HumanMessage("Explain what show.lua does"),
+    # HumanMessage("What does the code in `cat show.lua` do?"),
+    HumanMessage("Can you run show.lua for me?"), # Qwen3.6 runs `lua show.lua` and that fails => so it `ls -l` and corrects to find `show.py`
 ]
 
 from run_python import run_command, run_python
