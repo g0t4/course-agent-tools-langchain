3375d7c240df78ab0f4a9546f6345de8c7977795
Wes Higbee <wes.mcclure@gmail.com>
2026-04-23 00:25:22 -0500

    drop reasoning in tests

--- tools/show_test.py
+++ tools/show_test.py
@@ -9,8 +9,11 @@ from langchain_core.tools import tool
 from langchain_llama_server import ChatLlamaServer
 
 model = ChatLlamaServer(base_url="http://paxy:8012", api_key="",
+
     # Qwen3.6 via llama-server
-    # extra_body={"chat_template_kwargs": { "enable_thinking": False }}, \
+    extra_body={"chat_template_kwargs": { "enable_thinking": False }}, \
+    # comment out to test how reasoning displays
+
 )
 
 messages = [
