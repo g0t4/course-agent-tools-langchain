3548d699195017a7b177c265c56b230b6ebfd763
Wes Higbee <wes.mcclure@gmail.com>
2026-04-23 20:19:17 -0500

    first

--- tools/recover/coding.py
+++ tools/recover/coding.py
@@ -14,10 +14,10 @@ model = ChatLlamaServer(base_url="http://paxy:8012", api_key="",
 )
 
 messages = [
+    HumanMessage("Can you run show.lua for me?"), # Qwen3.6 runs `lua show.lua` and that fails => so it `ls -l` and corrects to find `show.py`
     # HumanMessage("Explain this project, in the current directory"),
     # HumanMessage("Read show.py and investigate how I display the tool call ID value... add a function to shorten the ID to a max of first 6 chars and use that to show the ID"),
     # HumanMessage("What does the code in `cat show.lua` do?"),
-    HumanMessage("Can you run show.lua for me?"), # Qwen3.6 runs `lua show.lua` and that fails => so it `ls -l` and corrects to find `show.py`
 ]
 
 from run_python import run_command, run_python
