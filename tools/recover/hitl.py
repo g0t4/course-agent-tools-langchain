835fd9f87e0370a6d1f4c0a94cf308cf88e77f12
Wes Higbee <wes.mcclure@gmail.com>
2026-04-26 19:32:33 -0500

    notes

--- tools/recover/hitl.py
+++ tools/recover/hitl.py
@@ -45,6 +45,8 @@ messages = [
     # HumanMessage("Read show.py and investigate how I display the tool call ID value... add a function to shorten the ID to a max of first 6 chars and use that to show the ID"),
 ]
 
+# FYI checkpointers: https://pypi.org/search/?q=langgraph-checkpoint
+# import { PostgresStore } from "@langchain/langgraph-checkpoint-postgres/store";
 checkpointer = InMemorySaver()
 agent = create_agent(
     model=model,
