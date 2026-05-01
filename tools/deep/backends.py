00d236058901b5a47ebb4420851ce36c8d1053e4
Wes Higbee <wes.mcclure@gmail.com>
2026-05-01 04:45:31 -0500

    what tools

---
+++ tools/deep/backends.py
@@ -0,0 +1,33 @@
+# auto reload module changes
+get_ipython().extension_manager.load_extension("autoreload")  # pyright: ignore
+get_ipython().run_line_magic('autoreload', 'complete --print')  # pyright: ignore
+
+import show
+from show import stream_messages
+
+import rich
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage, ToolMessage
+from langchain_llama_server import ChatLlamaServer
+from deepagents import create_deep_agent
+
+
+
+
+model = ChatLlamaServer(base_url="http://paxy:8012", api_key="",
+    # Qwen3.6 via llama-server
+    # * disable thinking + give explicit instructions to gen parallel tool calls to Qwen3.6
+    extra_body={"chat_template_kwargs": { "enable_thinking": False }}, \
+)
+
+messages = [
+    HumanMessage("What tools do you have?"),
+]
+
+agent = create_deep_agent(
+    model=model,
+)
+
+# %% 
+
+events = await stream_messages(agent, messages) # pyright: ignore
+
