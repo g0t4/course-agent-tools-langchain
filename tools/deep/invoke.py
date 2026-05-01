3720a78b13bfaf3032a1ad8b8f3f639b6c719882
Wes Higbee <wes.mcclure@gmail.com>
2026-05-01 02:30:52 -0500

    use this as my demo resume point to simplify the initial deep agents demos which were just about showing its still just an agent

--- tools/deep/invoke.py
+++ tools/deep/invoke.py
@@ -2,26 +2,21 @@
 get_ipython().extension_manager.load_extension("autoreload")  # pyright: ignore
 get_ipython().run_line_magic('autoreload', 'complete --print')  # pyright: ignore
 
-from typing import Type
-from deepagents.backends import FilesystemBackend, LocalShellBackend
-from deepagents.backends.protocol import SandboxBackendProtocol
-from langgraph.graph.state import RunnableConfig
-from langgraph.pregel.main import queue
-from langgraph.types import Command
-from pydantic import BaseModel, Field
+import show
+from show import stream_messages
+
 import rich
-from langchain.agents import create_agent
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage, ToolMessage
-from langchain_core.tools import BaseTool, tool
 from langchain_llama_server import ChatLlamaServer
-from langchain.agents.middleware import HumanInTheLoopMiddleware
-from langgraph.checkpoint.memory import InMemorySaver
 from deepagents import create_deep_agent
+
+
+
 
 model = ChatLlamaServer(base_url="http://paxy:8012", api_key="",
     # Qwen3.6 via llama-server
     # * disable thinking + give explicit instructions to gen parallel tool calls to Qwen3.6
-    # extra_body={"chat_template_kwargs": { "enable_thinking": False }}, \
+    extra_body={"chat_template_kwargs": { "enable_thinking": False }}, \
 )
 
 messages = [
@@ -30,11 +25,8 @@ messages = [
 
 agent = create_deep_agent(
     model=model,
+    system_prompt="Follow the user request exactly as written and do not reinterpret or optimize it.",
 )
-
-import show
-# show.last_events is populated even if you kill the trace (ctrl+c) or an exception interrupted it!
-from show import stream_messages
 
 events = await stream_messages(agent, messages) # pyright: ignore
 
